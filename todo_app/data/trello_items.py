027f4e81dcd58cb8c346fc8919c82c1b0b4c6484
Luke Woods <luke.woods@mclaren.com>
2021-02-08 16:05:43 +0000

    Finished base goals and moved creds to .env

---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import configparser
+import os
+
+#apiconfig = configparser.ConfigParser()
+#apiconfig.read('config.ini')
+credkey = os.environ.get('KEY')
+credtoken = os.environ.get('TOKEN')
+board_id_config = os.environ.get('BOARDID')
+
+class Trello_Card:
+    def __init__(self, id, name, idList):
+        self.id = id
+        self.name = name
+        self.idList = idList
+
+def get_trello_board_id():
+    trello_board_id = board_id_config
+
+    return trello_board_id
+
+def get_trello_cards():
+    board_id = get_trello_board_id()
+    getcardparams = {'key': credkey, 'token': credtoken}
+    cardresults = requests.get(f'https://api.trello.com/1/boards/{board_id}/cards', params=getcardparams)
+    cards = cardresults.json()
+    all_cards = []
+    for c in cards:
+        c = Trello_Card(c['id'], c['name'], c['idList'])
+        all_cards.append(c)
+
+    return all_cards
+
+def get_trello_lists():
+    board_id = get_trello_board_id()
+    getlistparams = {'key': credkey, 'token': credtoken}
+    listresults = requests.get(f'https://api.trello.com/1/boards/{board_id}/lists', params=getlistparams)
+    lists = listresults.json()
+
+    return lists
+
+def create_new_trello_card(card_name):
+    lists = get_trello_lists()
+    for l in lists:
+        if l['name'] == "To Do":
+            desired_list_id = l['id']
+    createcardparams = {'key': credkey, 'token': credtoken, 'idList': desired_list_id, 'name': card_name}
+    requests.post('https://api.trello.com/1/cards', params=createcardparams)
+
+def move_trello_card(card_id, new_list_id):
+    '''lists = get_trello_lists()
+    for l in lists:
+        if l['id'] == new_list_id:
+            desired_list_id = l['id']'''
+    movecardparams = {'key': credkey, 'token': credtoken, 'idList': new_list_id}
+    requests.put(f'https://api.trello.com/1/cards/{card_id}', params=movecardparams)
