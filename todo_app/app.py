8605997ea6b4c123c9b8f6602c231a773311da30
Luke Woods <luke.woods@mclaren.com>
2021-02-09 10:19:28 +0000

    Tidy code and finish

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, redirect
-from operator import itemgetter
 import requests
 import json
 import os
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, get_item, add_item, save_item, remove_item
 from todo_app.data.trello_items import get_trello_board_id, get_trello_cards, get_trello_lists, create_new_trello_card, move_trello_card
 
 app = Flask(__name__)
@@ -13,7 +11,6 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    #items = sorted(get_items(), key=itemgetter('status'), reverse=True)
     alllists = get_trello_lists()
     items = get_trello_cards()
     return render_template("index.html",items=items, lists=alllists)
