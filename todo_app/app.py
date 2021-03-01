a0a595867af8c15c357b9d6f7ffb4f58252cd44e
Luke Woods <luke.woods@mclaren.com>
2021-03-01 17:26:05 +0000

    Fixed the issue with items completed today, need to create a buttn to show older completed items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template, request, redirect
-import requests
-import json
-import os
+import requests, json, os, datetime
 
 from todo_app.flask_config import Config
-from todo_app.data.trello_items import get_trello_board_id, get_trello_cards, get_trello_lists, create_new_trello_card, move_trello_card
+from todo_app.data.trello_items import get_trello_board_id, get_trello_cards, get_trello_lists, create_new_trello_card, move_trello_card, ViewModel, get_trello_cards_from_list, get_trello_list_id, archive_trello_card
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    alllists = get_trello_lists()
+    alllistids = {'todo':get_trello_list_id('To Do'),'doing':get_trello_list_id('Doing'),'done':get_trello_list_id('Done')}
     items = get_trello_cards()
-    return render_template("index.html",items=items, lists=alllists)
+    item_view_model = ViewModel(items, alllistids)
+    return render_template('index.html', view_model=item_view_model)
 
 @app.route('/add_new', methods=['POST'])
 def addtolist():
     newitemtitle = request.form.get('title')
-    create_new_trello_card(newitemtitle)
+    newitemdescription = request.form.get('desc')
+    create_new_trello_card(newitemtitle, newitemdescription)
     return redirect(request.headers.get('Referer'))
 
 @app.route('/update', methods=['POST'])
@@ -36,6 +36,9 @@ def movetolist():
                 if l['name'] == "Done":
                     desired_list_id = l['id']
                     move_trello_card(card.id, desired_list_id)
+        if request.form.get("archivecheck_" + card.id) == card.id:
+            archive_trello_card(card.id)
+
     return redirect(request.headers.get('Referer'))
    
 
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,16 +1,71 @@
-import requests
-import json
-import os
+import requests, json, os, datetime
 
 credkey = os.environ.get('KEY')
 credtoken = os.environ.get('TOKEN')
 board_id_config = os.environ.get('BOARDID')
 
-class Trello_Card:
-    def __init__(self, id, name, idList):
+class TrelloCard:
+    def __init__(self, id, name, idList, due, description, modified):
         self.id = id
         self.name = name
         self.idList = idList
+        self.due = due
+        self.description = description
+        self.modified = modified
+
+class ViewModel:
+    def __init__(self, items, lists):
+        self._items = items
+        self._lists = lists
+
+    @property
+    def items(self):
+        return self._items
+    @property
+    def lists(self):
+        return self._lists
+    @property
+    def todo(self):
+        items = []
+        for item in self._items:
+            if item.idList == self._lists['todo']:
+                items.append(item)
+        return items
+    @property
+    def doing(self):
+        items = []
+        for item in self._items:
+            if item.idList == self._lists['doing']:
+                items.append(item)
+        return items
+    @property
+    def done(self):
+        items = []
+        for item in self._items:
+            if item.idList == self._lists['done']:
+                items.append(item)
+        return items
+    @property
+    def done_today(self):
+        items = []
+        for item in self._items:
+            modified_date = str(item.modified)
+            completed_date = modified_date.split(" ")
+            todays_date = datetime.date.today()
+            if item.idList == self._lists['done'] and completed_date[0] == str(todays_date):
+                items.append(item)
+        return items
+    @property
+    def done_before_today(self):
+        items = []
+        for item in self._items:
+            modified_date = str(item.modified)
+            completed_date = modified_date.split(" ")
+            todays_date = datetime.date.today()
+            if item.idList == self._lists['done'] and completed_date[0] != str(todays_date):
+                items.append(item)
+        return items
+
 
 def get_trello_board_id():
     trello_board_id = board_id_config
@@ -24,10 +79,41 @@ def get_trello_cards():
     cards = cardresults.json()
     all_cards = []
     for c in cards:
-        c = Trello_Card(c['id'], c['name'], c['idList'])
+        if c['due'] == None:
+            duedate = "No Due Date Specified"
+        else:
+            duedate = datetime.datetime.strptime(str(c['due']), '%Y-%m-%dT%H:%M:%S.%fZ')
+        item_modified_date = str(c['dateLastActivity'])
+        split_modified_date_from_milliseconds = item_modified_date.split('.')
+        modified_date = datetime.datetime.strptime(str(split_modified_date_from_milliseconds[0]), '%Y-%m-%dT%H:%M:%S')
+
+        c = TrelloCard(c['id'], c['name'], c['idList'], duedate, c['desc'], modified_date)
         all_cards.append(c)
 
     return all_cards
+
+def get_trello_cards_from_list(list):
+    alllists = get_trello_lists()
+    for l in alllists:
+        if l['name'] == list:
+            desired_list_id = l['id']
+    getcardfromlistparams = {'key': credkey, 'token': credtoken}
+    cardfromlistresults = requests.get(f'https://api.trello.com/1/lists/{desired_list_id}/cards', params=getcardfromlistparams)
+    cards = cardfromlistresults.json()
+    list_cards = []
+    for c in cards:
+        if c['due'] == None:
+            duedate = "No Due Date Specified"
+        else:
+            duedate = datetime.datetime.strptime(str(c['due']), '%Y-%m-%dT%H:%M:%S.%fZ')
+        item_modified_date = str(c['dateLastActivity'])
+        split_modified_date_from_milliseconds = item_modified_date.split('.')
+        modified_date = datetime.datetime.strptime(str(split_modified_date_from_milliseconds[0]), '%Y-%m-%dT%H:%M:%S')
+
+        c = TrelloCard(c['id'], c['name'], c['idList'], duedate, c['desc'], modified_date)
+        list_cards.append(c)
+
+    return list_cards
 
 def get_trello_lists():
     board_id = get_trello_board_id()
@@ -37,14 +123,28 @@ def get_trello_lists():
 
     return lists
 
-def create_new_trello_card(card_name):
+def get_trello_list_id(list_name):
+    alllists = get_trello_lists()
+    for l in alllists:
+        if l['name'] == list_name:
+            desired_list_id = l['id']
+    return desired_list_id
+
+def create_new_trello_card(card_name, card_description):
     lists = get_trello_lists()
+    duedate = str(datetime.datetime.today() + datetime.timedelta(days=30))
+    split_item_duedate = duedate.split(".")
+    newitemduedate = datetime.datetime.strptime(split_item_duedate[0], '%Y-%m-%d %H:%M:%S')
     for l in lists:
         if l['name'] == "To Do":
             desired_list_id = l['id']
-    createcardparams = {'key': credkey, 'token': credtoken, 'idList': desired_list_id, 'name': card_name}
+    createcardparams = {'key': credkey, 'token': credtoken, 'idList': desired_list_id, 'name': card_name, 'desc': card_description, 'due': str(newitemduedate)}
     requests.post('https://api.trello.com/1/cards', params=createcardparams)
 
 def move_trello_card(card_id, new_list_id):
     movecardparams = {'key': credkey, 'token': credtoken, 'idList': new_list_id}
     requests.put(f'https://api.trello.com/1/cards/{card_id}', params=movecardparams)
+
+def archive_trello_card(card_id):
+    archivecardparams = {'key': credkey, 'token': credtoken, 'closed': "true"}
+    requests.put(f'https://api.trello.com/1/cards/{card_id}', params=archivecardparams)
