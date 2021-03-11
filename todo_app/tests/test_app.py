562d3a40c6797621b4072a3aaa3484f54ce85322
Luke Woods <luke.woods@mclaren.com>
2021-03-11 15:30:46 +0000

    Completed Part 4 - assertions

--- todo_app/tests/test_app.py
+++ todo_app/tests/test_app.py
@@ -1,147 +1,26 @@
-import pytest, datetime, os, json
+import pytest, os, json
 from dotenv import find_dotenv, load_dotenv
 from todo_app.data.trello_items import TrelloCard, ViewModel
 import todo_app.app
 from unittest.mock import patch, Mock
 
-today = datetime.date.today()
 dummy_cards = [
     {
-        "id": "1",
+        "id": "60410ef575087c7bcb82dccb",
         "checkItemStates": None,
         "closed": False,
         "dateLastActivity": "2021-03-04T16:46:45.995Z",
         "desc": "testing desc",
         "descData": None,
         "dueReminder": None,
-        "idBoard": "123456ZYXWV",
-        "idList": "999",
+        "idBoard": "600fdeffdab98c51ec0aced6",
+        "idList": "600fdeffdab98c51ec0aced7",
         "idMembersVoted": [],
         "idShort": 26,
         "idAttachmentCover": None,
         "idLabels": [],
         "manualCoverAttachment": False,
-        "name": "Test Card 1",
-        "pos": 16384,
-        "shortLink": "hDebNQN6",
-        "isTemplate": False,
-        "cardRole": None,
-        "badges": {
-            "attachmentsByType": {
-                "trello": {
-                    "board": 0,
-                    "card": 0
-                }
-            },
-            "location": False,
-            "votes": 0,
-            "viewingMemberVoted": False,
-            "subscribed": False,
-            "fogbugz": "",
-            "checkItems": 0,
-            "checkItemsChecked": 0,
-            "checkItemsEarliestDue": None,
-            "comments": 0,
-            "attachments": 0,
-            "description": True,
-            "due": "2021-04-03T20:46:45.000Z",
-            "dueComplete": False,
-            "start": None
-        },
-        "dueComplete": False,
-        "due": "2021-04-03T20:46:45.000Z",
-        "idChecklists": [],
-        "idMembers": [],
-        "labels": [],
-        "shortUrl": "https://trello.com/c/hDebNQN6",
-        "start": None,
-        "subscribed": False,
-        "url": "https://trello.com/c/hDebNQN6/26-finish-project",
-        "cover": {
-            "idAttachment": None,
-            "color": None,
-            "idUploadedBackground": None,
-            "size": "normal",
-            "brightness": "light",
-            "idPlugin": None
-        }
-    },
-     {
-        "id": "1",
-        "checkItemStates": None,
-        "closed": False,
-        "dateLastActivity": "2021-03-04T16:46:45.995Z",
-        "desc": "testing desc",
-        "descData": None,
-        "dueReminder": None,
-        "idBoard": "123456ZYXWV",
-        "idList": "998",
-        "idMembersVoted": [],
-        "idShort": 26,
-        "idAttachmentCover": None,
-        "idLabels": [],
-        "manualCoverAttachment": False,
-        "name": "Test Card 2",
-        "pos": 16384,
-        "shortLink": "hDebNQN6",
-        "isTemplate": False,
-        "cardRole": None,
-        "badges": {
-            "attachmentsByType": {
-                "trello": {
-                    "board": 0,
-                    "card": 0
-                }
-            },
-            "location": False,
-            "votes": 0,
-            "viewingMemberVoted": False,
-            "subscribed": False,
-            "fogbugz": "",
-            "checkItems": 0,
-            "checkItemsChecked": 0,
-            "checkItemsEarliestDue": None,
-            "comments": 0,
-            "attachments": 0,
-            "description": True,
-            "due": "2021-04-03T20:46:45.000Z",
-            "dueComplete": False,
-            "start": None
-        },
-        "dueComplete": False,
-        "due": "2021-04-03T20:46:45.000Z",
-        "idChecklists": [],
-        "idMembers": [],
-        "labels": [],
-        "shortUrl": "https://trello.com/c/hDebNQN6",
-        "start": None,
-        "subscribed": False,
-        "url": "https://trello.com/c/hDebNQN6/26-finish-project",
-        "cover": {
-            "idAttachment": None,
-            "color": None,
-            "idUploadedBackground": None,
-            "size": "normal",
-            "brightness": "light",
-            "idPlugin": None
-        }
-    },
-     {
-        "id": "1",
-        "checkItemStates": None,
-        "closed": False,
-        "dateLastActivity": "2021-03-04T16:46:45.995Z",
-        "desc": "testing desc",
-        "descData": None,
-        "dueReminder": None,
-        "idBoard": "123456ZYXWV",
-        "idList": "997",
-        "idMembersVoted": [],
-        "idShort": 26,
-        "idAttachmentCover": None,
-        "idLabels": [],
-        "manualCoverAttachment": False,
-        "name": "Test Card 3",
+        "name": "Finish project",
         "pos": 16384,
         "shortLink": "hDebNQN6",
         "isTemplate": False,
@@ -193,19 +72,31 @@ sample_trello_cards_response = dummy_cards
 
 dummy_lists = [
     {
-        "id": 999,
+        "id": "600fdeffdab98c51ec0aced7",
         "name": "To Do",
-        "closed": False
+        "closed": False,
+        "pos": 16384,
+        "softLimit": None,
+        "idBoard": "600fdeffdab98c51ec0aced6",
+        "subscribed": False
     },
     {
-        "id": 998,
+        "id": "600fdeffdab98c51ec0aced8",
         "name": "Doing",
-        "closed": False
+        "closed": False,
+        "pos": 32768,
+        "softLimit": None,
+        "idBoard": "600fdeffdab98c51ec0aced6",
+        "subscribed": False
     },
     {
-        "id": 997,
+        "id": "600fdeffdab98c51ec0aced9",
         "name": "Done",
-        "closed": False
+        "closed": False,
+        "pos": 49152,
+        "softLimit": None,
+        "idBoard": "600fdeffdab98c51ec0aced6",
+        "subscribed": False
     }
 ]
 
@@ -223,6 +114,10 @@ def client():
 def test_index_page(mock_get_requests, client):
     mock_get_requests.side_effect = mock_trello_api
     response = client.get('/')
+
+    assert b'To Do' in response.data
+    assert b'Finish project' in response.data
+
 def mock_trello_api(url, params):
     BOARDID = os.environ.get('BOARDID')
     if url == f'https://api.trello.com/1/boards/{BOARDID}/lists':
